a9e13f254e287e79681bd193a08d2a3c81bdd7f0
GabrielPalhares28 <gabrielpalhares764@gmail.com>
2025-05-13 20:18:57 -0300

    ajuste 9

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -17,7 +18,9 @@ export default defineConfig({
             },
         }),
     ],
-    optimizeDeps: {
-        include: ['ziggy-js'],
+    resolve: {
+        alias: {
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist/index.esm.js'),
+        },
     },
 });
