12998a755cdc08f6427dd063ec26d51eed2c59c8
GabrielPalhares28 <gabrielpalhares764@gmail.com>
2025-05-13 19:16:47 -0300

    ajuste 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from 'ziggy-js';const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import ZiggyVue from '../../vendor/tightenco/ziggy/dist/vue.es.js';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
