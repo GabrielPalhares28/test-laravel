28dc0b3a9e90e1e60c2a7a8b1149ce01dbfe844c
GabrielPalhares28 <gabrielpalhares764@gmail.com>
2025-05-13 21:33:40 -0300

    ajuste ziggy

--- vue-frontend/vite.config.js
+++ vue-frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': '/src',
+      ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
     },
   },
 });
